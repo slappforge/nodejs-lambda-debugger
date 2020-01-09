5ab868f3d7eb7f8a02ecb6ab8083fcfaa41473a6
udith <udith@adroitlogic.com>
2020-01-09 10:59:53 +0530

    Enhanced local client to support VS Code

--- client/lib/client.js
+++ client/lib/client.js
@@ -4,6 +4,22 @@ const {logIfVerbose, printGreenWTime, printRedWTime, printYellowWTime, printYell
 
 const DEBUGGER_PORT = 9249;
 
+const HTTP_REQUEST_PATHS = {
+    JSON: '/json',                  // WebStorm/IDEA debugger send only this
+    JSON_VERSION: '/json/version',  // VS Code first sends this
+    JSON_LIST: '/json/list'         // VS Code secondly sends this
+};
+
+const METHOD_NAMES = {
+    RUN_IF_WAITING: 'Runtime.runIfWaitingForDebugger',
+    RUN: 'Runtime.run',
+    SET_BRK_PT: 'Debugger.setBreakpointByUrl',
+    GET_BRK_PTS: 'Debugger.getPossibleBreakpoints',
+    LOG_ENABLE: 'Log.enable',
+    NODE_WRK_ENABLE: 'NodeWorker.enable',
+    CONSOLE_ENABLE: 'Console.enable'
+};
+
 module.exports = {
     startClient: (host, port, funcID, authKey, authSecret, verbose) => {
 
@@ -19,38 +35,69 @@ module.exports = {
         let bSocket;
         const server = http.createServer((req, res) => {
 
-            if (!(bSocket && (bSocket.readyState === WebSocket.OPEN))) {
-                printGreenWTime('Connecting to debug broker server...');
-                bSocket = new WebSocket(`ws://${authKey}:${authSecret}@${host}:${port}/${authKey}-${funcID}`);
-                // bSocket = new WebSocket(`ws://${authKey}:${authSecret}@${host}:${port}/${funcID}`);
-            }
+            let requestURL = req.url;
 
-            bSocket.on('open', function () {
-                printGreenWTime('Connected to debug broker server');
+            if (requestURL === HTTP_REQUEST_PATHS.JSON_VERSION) {
                 res.end(JSON.stringify(
                     [
                         {
-                            "description": "node.js instance",
-                            "id": funcID,
-                            "title": "index.js",
-                            "type": "node",
-                            "webSocketDebuggerUrl": `ws://127.0.0.1:${DEBUGGER_PORT}/${funcID}`
+                            "Browser": "node.js",
+                            "Protocol-Version": "1.1"
                         }
                     ]
                 ));
-            });
 
+            } else if ((requestURL === HTTP_REQUEST_PATHS.JSON) || (requestURL === HTTP_REQUEST_PATHS.JSON_LIST)) {
+
+                if (!(bSocket && (bSocket.readyState === WebSocket.OPEN))) {
+                    printGreenWTime('Connecting to debug broker server...');
+                    bSocket = new WebSocket(`ws://${authKey}:${authSecret}@${host}:${port}/${authKey}-${funcID}`);
+                }
+
+                bSocket.on('open', function () {
+                    printGreenWTime('Connected to debug broker server');
+                    res.end(JSON.stringify(
+                        [
+                            {
+                                "description": "node.js instance",
+                                "id": funcID,
+                                "title": "index.js",
+                                "type": "node",
+                                "webSocketDebuggerUrl": `ws://127.0.0.1:${DEBUGGER_PORT}/${funcID}`
+                            }
+                        ]
+                    ));
+                });
+
+            } else {
+                printRedWTime("Unknown request type");
+                res.end(JSON.stringify({}));
+            }
         });
         const userServer = new WebSocket.Server({server});
         userServer.on('connection', function connection(ws) {
             printGreenWTime('Debugger connected');
 
             ws.on('message', function incoming(message) {
-                logIfVerbose(verbose, 'C-2-B', '>>>', message);
-                if (bSocket.readyState === WebSocket.OPEN) {
-                    bSocket.send(message);
-                } else {
-                    bSocket.terminate();
+                let msgObj = JSON.parse(message);
+                let method = msgObj.method;
+
+                if (method !== METHOD_NAMES.RUN) {
+                    if ((method === METHOD_NAMES.LOG_ENABLE)) {
+                        let newMsg = {
+                            ...msgObj,
+                            method: METHOD_NAMES.SET_BRK_PT,
+                            params: {"lineNumber": 1, "urlRegex": ".*"}
+                        };
+                        message = JSON.stringify(newMsg);
+                    }
+
+                    logIfVerbose(verbose, 'C-2-B', '>>>', message);
+                    if (bSocket.readyState === WebSocket.OPEN) {
+                        bSocket.send(message);
+                    } else {
+                        bSocket.terminate();
+                    }
                 }
             });
 
