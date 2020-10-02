74493c3b43fd82ff96d0115383392104e00aa5f1
udith <udith@adroitlogic.com>
2020-10-02 15:11:41 +0530

    Removed ap-east-1 region from deployment script

--- layer/deploy.sh
+++ layer/deploy.sh
@@ -11,8 +11,8 @@ read -r -p "AWS profile name: " profileName
 layerName=${LAYER_NAME}-${LAYER_BUILD_VERSION}
 
 # shellcheck disable=SC2039
-lambdaRegions=(us-east-1 us-east-2 us-west-1 us-west-2 ap-east-1 ap-south-1 ap-northeast-2 ap-southeast-1
-ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 eu-north-1 sa-east-1)
+lambdaRegions=(us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2
+ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 eu-north-1 sa-east-1)
 
 # shellcheck disable=SC2039
 echo "Publishing layer: ${layerName} ..."
